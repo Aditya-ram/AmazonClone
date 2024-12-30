aae3db52f4939fad6ab6c97eeb86bd5d569a3ca8
Nirvaan47 <nirvaanshetty47@gmail.com>
2024-12-30 20:39:52 +0530

    Added a navbar

---
+++ amazon-react/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "./SearchBar/SearchBar";
+
+function Header() {
+  return (
+    <div id="amazon_header">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Header;
---
+++ amazon-react/src/Components/Header/SearchBar/SearchBar.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+
+const SearchBar = () => {
+  return <input type="text" id="amazon_search_bar" />;
+};
+
+export default SearchBar;
