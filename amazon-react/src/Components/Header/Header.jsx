e0bdbec5bdb552c4d49ba9bdeaf589939e87977a
Nirvaan47 <nirvaanshetty47@gmail.com>
2024-12-31 16:42:32 +0530

    Completed first half of header.

--- amazon-react/src/Components/Header/Header.jsx
+++ amazon-react/src/Components/Header/Header.jsx
@@ -1,9 +1,26 @@
-import SearchBar from "./SearchBar/SearchBar";
+import amazonImage from "../../assets/amazonLogo.png";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import "../Header/Header.css";
 
 function Header() {
   return (
-    <div id="amazon_header">
-      <SearchBar />
+    <div className="amazon_header">
+      <div className="image_suffix">
+        <img src={amazonImage} alt="" />
+        <span id="amazon_suffix">.in</span>
+      </div>
+      <div className="amazon_header_location">
+        <LocationOnOutlinedIcon />
+        <div className="location_text">
+          <div>Delivering to Bengaluru</div>
+          <div id="update_location_text">Update Location</div>
+        </div>
+      </div>
+      <div className="header_dropdown">
+        <span>All</span>
+        <ArrowDropDownIcon />
+      </div>
     </div>
   );
 }
--- amazon-react/src/Components/Header/SearchBar/SearchBar.jsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-const SearchBar = () => {
-  return <input type="text" id="amazon_search_bar" />;
-};
-
-export default SearchBar;
