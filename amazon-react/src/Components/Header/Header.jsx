63c961d316789ceb65b0324939cafc176c6b6afd
Nirvaan47 <nirvaanshetty47@gmail.com>
2025-01-01 15:30:35 +0530

    completed the header of amazon-clone.

--- amazon-react/src/Components/Header/Header.jsx
+++ amazon-react/src/Components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import amazonImage from "../../assets/amazonLogo.png";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ProductionQuantityLimitsOutlinedIcon from "@mui/icons-material/ProductionQuantityLimitsOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 import "../Header/Header.css";
 
 function Header() {
@@ -17,9 +19,31 @@ function Header() {
           <div id="update_location_text">Update Location</div>
         </div>
       </div>
-      <div className="header_dropdown">
-        <span>All</span>
-        <ArrowDropDownIcon />
+      <div className="amazon_search_bar">
+        <input type="text" placeholder="Search Here" className="input_bar" />
+        <div className="amazon_search_bar_icon">
+          <SearchIcon />
+        </div>
+      </div>
+      <div className="amazon_account_list">
+        <div>Hello, sign in</div>
+        <div className="amazon_account">
+          <div>Account and lists</div>
+          <span>
+            <ArrowDropDownIcon />
+          </span>
+        </div>
+      </div>
+      <div className="amazon_returns_and_orders">
+        <div className="amazon_returns">Returns</div>
+        <div className="amazon_orders">& Orders</div>
+      </div>
+      <div className="amazon_cart_wrapper">
+        <div className="amazon_cart">
+          <span>0</span>
+          <ProductionQuantityLimitsOutlinedIcon />
+        </div>
+        <span>Cart</span>
       </div>
     </div>
   );
