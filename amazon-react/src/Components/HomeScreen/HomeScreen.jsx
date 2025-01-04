2ed3a2d586632a6c9aa51b31c4a7d74e8b434bb9
adiram2001 <adi2001ram@gmail.com>
2025-01-04 12:10:50 +0530

    Provided space between home details and footer

--- amazon-react/src/Components/HomeScreen/HomeScreen.jsx
+++ amazon-react/src/Components/HomeScreen/HomeScreen.jsx
@@ -2,10 +2,11 @@
 import Footer from "../Footer/Footer";
 import HomeBanner from "./HomeBanner/HomeBanner";
 import HomeDetails from "./HomeDetails/HomeDetails";
+import '../HomeScreen/HomeScreen.css'
 
 const HomeScreen = () => {
   return (
-    <div>
+    <div id="HomeScreenWrapper">
       <HomeBanner />
       <HomeDetails />
       <Footer />
