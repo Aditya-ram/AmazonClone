192ca10254f52547e8847a84b3c4efee01e16b21
Nirvaan47 <nirvaanshetty47@gmail.com>
2025-01-05 13:41:42 +0530

    Working on container of the Products page.

--- amazon-react/src/Components/Products/Products.jsx
+++ amazon-react/src/Components/Products/Products.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import '../Products/products.css'
-import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack';
+import React from "react";
+import "../Products/products.css";
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
+
+import ProductsArray from "../../productPageData.json";
+
+import laptopImage from "../../assets/laptopProductImage.png";
 
 const Products = () => {
-    const Arr =['Mobiles & Accessories', 'Laptops and Accessories','TV & Home Entertainment','Audio','Cameras','Computer Peripherals','Smart  Technology','Musical Instruments','Office & Stationary']
+  const Arr = [
+    "Mobiles & Accessories",
+    "Laptops and Accessories",
+    "TV & Home Entertainment",
+    "Audio",
+    "Cameras",
+    "Computer Peripherals",
+    "Smart  Technology",
+    "Musical Instruments",
+    "Office & Stationary",
+  ];
   return (
     <>
       <div className="products_navBar">
         <div className="Electronics">Electronics</div>
         {Arr.map((item, ind) => {
-          return <div className="Products_NavBar_Items">{item}</div>;
+          return (
+            <div key={ind} className="Products_NavBar_Items">
+              {item}
+            </div>
+          );
         })}
       </div>
       <div className="main_content">
@@ -43,10 +61,21 @@ const Products = () => {
           <div className="items">Amazon Prime</div>
           <div className="items">Average Customer Review</div>
         </div>
-        <div className="Prodcuts_Main"></div>
+        <div className="products_container">
+          {ProductsArray.product.map((product, index) => {
+            return (
+              <div key={index} className="products_card">
+                <div className="products_image">
+                  <img className="laptop_image" src={product.imgs} alt="" />
+                </div>
+                <div className="products_description">{product.itemTitle}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
-}
+};
 
-export default Products
+export default Products;
